75a2b6faa5334c081c5e86b0b41161ec19f8ee5a
Jon Gunter <jon@jongunter.com>
2016-07-09 18:28:28 -0600

    Update index.js
    Add voice response

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res) {
 router.get('/callbox', function(req, res) {
   var twiml = new twilio.TwimlResponse();
 
+  twiml.say({voice:'man'}, 'Thanks for calling. I\'ll connect you a with resident shortly')
   twiml.dial(process.env.CALLBOX_NUMBER);
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
