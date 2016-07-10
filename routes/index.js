6dc2da394847a7bd2d7903973583537e2b0958f9
Jon Gunter <jon@jongunter.com>
2016-07-09 18:38:46 -0600

    Change time limi

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/callbox', function(req, res) {
   var twiml = new twilio.TwimlResponse();
 
   twiml.say({voice:'woman'}, 'Thanks for calling apartment ' + process.env.APARTMENT_NUMBER + '. I\'ll connect you with a resident shortly.')
-  twiml.dial(process.env.CALLBOX_NUMBER);
+  twiml.dial({timeLimit: 180}, process.env.CALLBOX_NUMBER);
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
